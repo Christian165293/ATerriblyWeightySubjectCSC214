4331ac3ae74788ac998ea3cdb85bdf7c69963a2f
Christian165293 <christian.bourquin05@gmail.com>
2025-01-24 20:26:19 -0600

    Finished Project, fixed the warnings by adding a Converter Object and removing static from the methods within Converter.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
+        Converter converter1 = new Converter();
         System.out.print("\nHow many ounces do you have? ");
         int userOuncesInput = scanner1.nextInt();
         System.out.println("Thank you! Converting ounces to pounds.\n");
-        System.out.println(userOuncesInput + " oz is equivalent to " + Converter.toPoundsAndOunces(userOuncesInput));
-        System.out.println(userOuncesInput + " oz is equivalent to " + Converter.toPounds(userOuncesInput) + "\n");
+        System.out.println(userOuncesInput + " oz is equivalent to " + converter1.toPoundsAndOunces(userOuncesInput));
+        System.out.println(userOuncesInput + " oz is equivalent to " + converter1.toPounds(userOuncesInput) + "\n");
     }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,12 +1,12 @@
 package org.example;
 
 public class Converter {
-    public static String toPounds(int inputOunces) {
+    public String toPounds(int inputOunces) {
         double lbsOutput = inputOunces / 16.0;
         return String.format("%.4f lbs", lbsOutput);
     }
 
-    public static String toPoundsAndOunces(int inputOunces) {
+    public String toPoundsAndOunces(int inputOunces) {
         int lbsOutput = inputOunces / 16;
         int ouncesOutput = inputOunces % 16;
         return String.format("%d lbs %d oz", lbsOutput, ouncesOutput);
