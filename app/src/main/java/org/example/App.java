8b3b7a77c484acab1b9afca4e52caf2dba6ad903
Christian165293 <christian.bourquin05@gmail.com>
2025-01-24 19:32:57 -0600

    Code works and does as it should but is throwing some warnings which need to be fixed.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("\nHow many ounces do you have? ");
+        int userOuncesInput = scanner1.nextInt();
+        System.out.println("Thank you! Converting ounces to pounds.\n");
+        System.out.println(userOuncesInput + " oz is equivalent to " + Converter.toPoundsAndOunces(userOuncesInput));
+        System.out.println(userOuncesInput + " oz is equivalent to " + Converter.toPounds(userOuncesInput) + "\n");
+    }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+    public static String toPounds(int inputOunces) {
+        double lbsOutput = inputOunces / 16.0;
+        return String.format("%.4f lbs", lbsOutput);
+    }
+
+    public static String toPoundsAndOunces(int inputOunces) {
+        int lbsOutput = inputOunces / 16;
+        int ouncesOutput = inputOunces % 16;
+        return String.format("%d lbs %d oz", lbsOutput, ouncesOutput);
+    }
 }
